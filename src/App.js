6f7e6bf8ade1b785b176403c08995b08fa0c2dcf
Akogwu Emma <akogwu.emmanuel24@gmial.com>
2020-12-12 16:09:38 +0100

    main chakra practice file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header'
+
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <h6>i am well connected</h6>
+    )
+}
+
+export default Header;
